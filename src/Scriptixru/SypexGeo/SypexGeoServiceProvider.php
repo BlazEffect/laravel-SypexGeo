b4bb4be1060b6efe298d3ca51647484ce340778a
Андрей Дмитракович <dmitrakovich.andrey@yandex.by>
2020-10-05 12:22:13 +0300

    update for laravel 7

--- src/Scriptixru/SypexGeo/SypexGeoServiceProvider.php
+++ src/Scriptixru/SypexGeo/SypexGeoServiceProvider.php
@@ -31,8 +31,7 @@ class SypexGeoServiceProvider extends ServiceProvider {
 	public function register()
 	{
 		// Register providers.
-		$this->app['sypexgeo'] = $this->app->share(function($app)
-		{
+		$this->app->singleton('sypexgeo', function($app) {
             $sypexConfig = $app['config'];
             $sypexConfigType = $sypexConfig->get('sypexgeo.sypexgeo.type', array());
             $sypexConfigPath = $sypexConfig->get('sypexgeo.sypexgeo.path', array());
